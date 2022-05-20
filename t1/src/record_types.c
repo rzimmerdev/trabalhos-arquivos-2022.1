77d9d70344e88fb29c1fe45ac3aa163bdc0352c1
Dani <dani.modesti@usp.br>
2022-05-20 20:11:17 -0300

    Adicionei as funções para cada tipo de arquivo no .c e coloquei os cabeçalhos no .h

--- t1/src/record_types.c
+++ t1/src/record_types.c
@@ -5,14 +5,55 @@
 #include "../lib/record.h"
 #include "../lib/dataframe.h"
 
+record *create_header_fixed() {
+    // Criando o cabecalho do arquivo de tipo 1 ---
+
+    int field_amt = 15;
+    // Tamanho de cada campo no registro:
+    int field_sizes[] = {1, 1, 40, 22, 19, 24, 8, 1, 16, 1, 18, 1, 19, 1, 1};
+    int type_sizes[] = {1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4};
+
+    return create_record(field_amt, field_sizes, type_sizes);
+}
+
+record *create_header_variable() {
+    // Criando o cabecalho do arquivo de tipo 2 ---
+
+    int field_amt = 15;
+    // Tamanho de cada campo no registro:
+    int field_sizes[] = {1, 1, 40, 22, 19, 24, 8, 1, 16, 1, 18, 1, 19, 1, 1};
+    int type_sizes[] = {1, 8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8, 4};
+
+    return create_record(field_amt, field_sizes, type_sizes);
+}
+
+record *create_data_fixed() {
+    // Criando os dados do arquivo de tipo 1 ---
+
+    int field_amt = 15;
+    // Tamanho de cada campo no registro:
+    int field_sizes[] = {1, 1, 1, 1, 1, 2, 1, 1, VAR_SIZE, 1, 1, VAR_SIZE, 1, 1, VAR_SIZE};
+    int type_sizes[] = {1, 4, 4, 4, 4, 1, 4, 1, 1, 4, 1, 1, 4, 1, 1};
+
+    return create_record(field_amt, field_sizes, type_sizes);
+}
+
+record *create_data_variable() {
+    // Criando os dados do arquivo de tipo 2 ---
+
+    int field_amt = 16;
+    // Tamanho de cada campo no registro:
+    int field_sizes[] = {1, 1, 1, 1, 1, 1, 2, 1, 1, VAR_SIZE, 1, 1, VAR_SIZE, 1, 1, VAR_SIZE};
+    int type_sizes[] = {1, 4, 8, 4, 4, 4, 1, 4, 1, 1, 4, 1, 1, 4, 1, 1};
+
+    return create_record(field_amt, field_sizes, type_sizes);
+}
 
 void csv_to_record_fixed(FILE *csv, FILE *dest, record *header_template, record *data_template) {
-
     record *header = read_record(csv, header_template, ',');
 
 }
 
 void csv_to_record_variable() {
-
 
 }
--- t1/src/record_types.h
+++ t1/src/record_types.h
@@ -6,40 +6,16 @@
 
 #include "../lib/record.h"
 
-// Criando o cabecalho do arquivo de tipo 1 (header1) ---
+record *create_header_fixed();
 
-int header1_field_amt = 15;
-// Tamanho de cada campo no registro:
-int header1_field_sizes[] = {1, 1, 40, 22, 19, 24, 8, 1, 16, 1, 18, 1, 19, 1, 1};
-int header1_type_sizes[] = {1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4};
+record *create_header_variable();
 
-record *type1_header = create_record(total_fields, field_sizes, type_sizes);
+record *create_data_fixed();
 
-// Criando os dados do arquivo de tipo 1 (data1) ---
+record *create_data_variable();
 
-int data1_field_amt = 15;
-// Tamanho de cada campo no registro:
-int data1_field_sizes[] = {1, 1, 1, 1, 1, 2, 1, 1, VAR_SIZE, 1, 1, VAR_SIZE, 1, 1, VAR_SIZE};
-int data1_type_sizes[] = {1, 4, 4, 4, 4, 1, 4, 1, 1, 4, 1, 1, 4, 1, 1};
+void csv_to_record_fixed(FILE *csv, FILE *dest, record *header_template, record *data_template);
 
-record *type1_data = create_record(data1_field_amt, data1_field_sizes, data1_type_sizes);
-
-// Criando o cabecalho do arquivo de tipo 2 (header2) ---
-
-int header2_field_amt = 15;
-// Tamanho de cada campo no registro:
-int header2_field_sizes[] = {1, 1, 40, 22, 19, 24, 8, 1, 16, 1, 18, 1, 19, 1, 1};
-int header2_type_sizes[] = {1, 8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8, 4};
-
-record *type2_header = create_record(header2_field_amt, header2_field_sizes, header2_type_sizes);
-
-// Criando o cabecalho do arquivo de tipo 2 (data2) ---
-
-int data2_field_amt = 16;
-// Tamanho de cada campo no registro:
-int data2_field_sizes[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, VAR_SIZE, 1, 1, VAR_SIZE, 1, 1, VAR_SIZE};
-int data2_type_sizes[] = {1, 4, 8, 4, 4, 4, 2, 4, 1, 1, 4, 1, 1, 4, 1, 1};
-
-record *type2_data = create_record(data2_field_amt, data2_field_sizes, data2_type_sizes);
+void csv_to_record_variable();
 
 #endif
