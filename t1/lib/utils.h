50279a69c4cdffc324ad7997cd87fc04efd254dc
rzimmerdev <rzimmerdev@gmail.com>
2022-05-20 20:29:16 -0300

    Adicionei utilidades para strings e escaneamento

---
+++ t1/lib/utils.h
@@ -0,0 +1,14 @@
+//
+// Created by rzimmerdev on 20/05/2022.
+//
+
+#ifndef T1_UTILS_H
+#define T1_UTILS_H
+
+char *scan_word();
+
+void binarioNaTela(char *nomeArquivoBinario);
+
+void scan_quote_string(char *str);
+
+#endif //T1_UTILS_H
