61308e9596a024105a2883bcc406df882ad4b5d9
Dani <dani.modesti@usp.br>
2022-05-20 17:12:10 -0300

    Adicionando a estrutura do switch na main :)

--- t1/src/main.c
+++ t1/src/main.c
@@ -1,11 +1,14 @@
-//----------------------------------------//
-// Project developed by Rafael Zimmer     //
-// nUsp: 12542612                         //
-//                                        //
-// Exercise for writing and reading from  //
-// files in C, for File Organization      //
-// course at Usp.                         //
-//----------------------------------------//
+/*
+ * Nomes dos integrantes da equipe (G1): Danielle Modesti e Rafael Zimmer
+ * Nos USP: 12543544 e 12542612
+ * Disciplina: Organizacao de Arquivos - 3o semestre (2022.1)
+ * Primeiro Trabalho Pratico - frota de veiculos no Brasil.
+ *
+ * Este trabalho tem como  objetivo armazenar dados em arquivos binarios de
+ * acordo com organizacoes de campos e registros diferentes, bem como
+ * desenvolver funcionalidades para recuperar dados desses arquivos.
+ * 
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,8 +17,28 @@
 #include "../lib/dataframe.h"
 #include "../lib/record.h"
 
+// Funcionalidades do trabalho
+typedef enum Command_t {
+    CREATE_TABLE = 1,
+    SELECT = 2,
+    SELECT_WHERE = 3,
+    SELECT_ID = 4
+} command;
 
 int main() {
+    int option;
+    scanf("%d", &option);
+
+    switch ((command)option) {
+        case CREATE_TABLE:
+            break;
+        case SELECT:
+            break;
+        case SELECT_WHERE:
+            break;
+        case SELECT_ID:
+            break;
+    }
 
     int total_fields = 7;
     // Tamanho de cada campo no registro:
