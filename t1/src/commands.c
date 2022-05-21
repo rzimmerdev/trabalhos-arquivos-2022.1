d8b2062720aab348dbb6153aed89ba82a539c964
Dani <dani.modesti@usp.br>
2022-05-20 22:13:17 -0300

    Funcionalidade 1 dos comandos

--- t1/src/commands.c
+++ t1/src/commands.c
@@ -2,21 +2,45 @@
 #include <stdio.h>
 
 #include "commands.h"
+#include "record_types.h"
 
 void create_table_command() {
     // Ler a entrada
     char *csv_filename = scan_word();
     char *out_filename = scan_word();
 
+    
+    FILE *csvfile_ptr = fopen(csv_filename, "r");
     free(csv_filename);
+
+    FILE *binfile_ptr = fopen(out_filename, "wb");
     free(out_filename);
+
+    if (!(csvfile_ptr && binfile_ptr)) {
+        printf("Falha no processamento do arquivo.\n");
+    }
+
+    csv_to_record_fixed(csvfile_ptr, binfile_ptr);
+
+    fclose(csvfile_ptr);
+    fclose(binfile_ptr);
 }
 
 void select_command() {
     // Ler a entrada
     char *bin_filename = scan_word();
 
-    free(bin_filename);
+    FILE *file_ptr = NULL;
+    file_ptr = fopen(bin_filename, "rb");
+
+    if (!file_ptr) {
+        printf("macaco");
+    }
+    
+
+    if (bin_filename) {
+        free(bin_filename);
+    }
 }
 
 void select_where_command() {
