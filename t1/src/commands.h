441ec0e89dbf3e710fddbd9a31f40188010432ed
Dani <dani.modesti@usp.br>
2022-05-20 21:33:07 -0300

    Separando as funcionalidades em novo arquivo.

---
+++ t1/src/commands.h
@@ -0,0 +1,14 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#include "../lib/utils.h"
+
+void create_table_command();
+
+void select_command();
+
+void select_where_command();
+
+void select_id_command();
+
+#endif
