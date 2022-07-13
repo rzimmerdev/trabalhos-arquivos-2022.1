0106c9d1082a1e7782fada09fca8fe1222031570
Dani <dani.modesti@usp.br>
2022-07-12 22:56:55 -0300

    Modificações na struct do nó da b-tree

--- lib/index/tree_index.h
+++ lib/index/tree_index.h
@@ -2,8 +2,8 @@
 // Created by rzimmerdev on 11/07/2022.
 //
 
-#ifndef FAST_INDEX_H
-#define FAST_INDEX_H
+#ifndef TREE_INDEX_H
+#define TREE_INDEX_H
 
 typedef struct TreeHeader_t {
 
@@ -16,20 +16,22 @@ typedef struct TreeHeader_t {
 
 } tree_header;
 
+typedef struct Key_t {
+    int id;
+    int rrn;
+    long int byteoffset;
+} key;
 
 typedef struct TreeNode_t {
 
     char type;
     int num_keys;
 
-    int keys[3];
-
-    int rrns[3];
-    long int byteoffsets[3];
+    key keys[3];
 
     int children[4];
 } tree_node;
 
 int tree_find_by_id(FILE *stream, int id, bool is_fixed);
 
-#endif //FAST_INDEX_H
+#endif //TREE_INDEX_H
